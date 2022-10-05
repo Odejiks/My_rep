de5fb55df237e10902dcbbe19752035afad7cb55
Odejiks <odejiks@gmail.com>
2022-10-05 15:58:52 +0300

    Add new file

---
+++ HW 7 /Kosmulya_O_les_1.py
@@ -0,0 +1,22 @@
+# # Калькулятор
+# number_1 = input("Введите первое число: ")
+# number_2 = input("введите второе число: ")
+# opera = input("введите опаратора: ")
+# if opera != "+" and opera != "-" and opera != "*" and opera != "/" :
+#     print("неправильный оператор")
+#     exit()
+# if number_1.isdigit():
+#     number_1 = int(number_1)
+# if number_2.isdigit():
+#     number_2 = int(number_2)
+# if opera == "+" :
+#     print(number_1 + number_2)
+# elif opera == "-" :
+#     print(number_1 - number_2)
+# elif opera == "*" :
+#     print(number_1 * number_2)
+# elif opera == "/" :
+#     if (number_2 != 0):
+#         print(number_1 / number_2)
+#     else :
+#         print("erro")
